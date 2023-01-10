922cca2e7531e464ba9b774d706b789bf2897099
Bharat Kumar <bharattoday9@gmail.com>
2023-01-10 08:45:22 +0000

    homepage

--- src/Templates/home.php
+++ src/Templates/home.php
@@ -1,1 +1,3 @@
-Oh shit it works!
+<h1>Welcome to PHPLover</h1>
+
+<p>PHPLover is a simple PHP Micro Framework for newbies to learn how to work with any PHP Framework, be it Laravel, Codeigniter, or Symfony or any other one</p>
